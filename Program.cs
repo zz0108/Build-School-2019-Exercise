36b9f81a378c87b2e9053ac169a325a21a3f2573
zz0108 <58735664+zz0108@users.noreply.github.com>
2020-02-14 17:17:48 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 費式數列
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int previous = 1;
+            int thelatter = 1;
+            int answer = 1;
+            Console.Write("1 + ");
+            while (true)
+            {
+                if(answer >= 1836311903)
+                {
+                    break;
+                }
+                else
+                {
+                    answer = thelatter + previous;
+                    Console.Write($"{thelatter}");
+                    if(answer < 1836311903)
+                    {
+                        Console.Write(" + ");
+                    }
+                    previous = thelatter;
+                    thelatter = answer;
+                }
+            }
+            Console.Write($" = {answer}");
+            Console.ReadLine();
+        }
+    }
+}
